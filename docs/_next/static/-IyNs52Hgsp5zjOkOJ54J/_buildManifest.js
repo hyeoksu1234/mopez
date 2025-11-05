5c96b730a7075fccb346f4f7bb65aa2e314211f5
masterpiece-alliance <leeyoon@ma-cc.co.kr>
2025-11-06 02:38:35 +0900

    chore: update docs export

---
+++ docs/_next/static/-IyNs52Hgsp5zjOkOJ54J/_buildManifest.js
@@ -0,0 +1,19 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/d87e0abcd33e5b6c.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/mopez//_next/:path+",
+        "destination": "/mopez/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
